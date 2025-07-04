7a60c16406727ef1bbaea883cc8be1e5644aa09a
Yongce Liu <airrighttime@outlook.com>
2025-07-04 17:00:57 +0800

    update

--- deploy.py
+++ deploy.py
@@ -6,18 +6,8 @@ from unitree_sdk2py.core import channel  # Pub, Sub, FactoryInitializer
 from unitree_sdk2py.go2.sport import sport_client
 
 # policy utils
-from utils.helpers import class_to_dict
-
-# rl-training configs
-from utils.task_registry import task_registry
-from configs.go2_constraint_him import (
-    Go2ConstraintHimRoughCfg,
-    Go2ConstraintHimRoughCfgPPO,
-)
-from envs import LeggedRobot
 from modules import ActorCriticRMA
-from utils import get_args
-
+from tools.utils import Policy, class_to_dict
 
 class trajectoryPublisher:
     def __init__(self):
@@ -36,40 +26,36 @@ class Dog:
         pass
 
 
-def load_policy(args, policy_path):
-    task_registry.register(
-        name=args.task,
-        task_class=LeggedRobot,
-        env_cfg=Go2ConstraintHimRoughCfg(),
-        train_cfg=Go2ConstraintHimRoughCfgPPO(),
-    )
-    env_cfg, train_cfg = task_registry.get_cfgs(name=args.task)
-    env, _ = task_registry.make_env(name=args.task, args=args, env_cfg=env_cfg)
+def load_policy(policy_path, device="cpu"):
+    n_scan = 187
+    n_priv_latent =  4 + 1 + 12 + 12 + 12 + 6 + 1 + 4 + 1 - 3 + 3 - 3 + 4 - 7
+    n_proprio = 45 + 3
+    history_len = 10
+    n_actions = 12
+    num_observations = n_proprio + n_scan + history_len*n_proprio + n_priv_latent
 
-    policy_cfg_dict = class_to_dict(train_cfg.policy)
-    runner_cfg_dict = class_to_dict(train_cfg.runner)
-    actor_critic_class = eval(runner_cfg_dict["policy_class_name"])
-    policy: ActorCriticRMA = actor_critic_class(
-        env.cfg.env.n_proprio,
-        env.cfg.env.n_scan,
-        env.num_obs,
-        env.cfg.env.n_priv_latent,
-        env.cfg.env.history_len,
-        env.num_actions,
+    _policy = Policy()
+    policy_cfg_dict = class_to_dict(_policy)
+    policy= ActorCriticRMA(
+        num_prop = n_proprio,
+        num_scan = n_scan,
+        num_critic_obs = num_observations,
+        num_priv_latent = n_priv_latent,
+        num_hist = history_len,
+        num_actions = n_actions,
         **policy_cfg_dict,
     )
     model_dict = torch.load(policy_path)
     policy.load_state_dict(model_dict["model_state_dict"])
     policy.half()
     policy.eval()
-    policy = policy.to(env.device)
-    policy.save_torch_jit_policy("model.pt", env.device)
+    policy = policy.to(device)
+    policy.save_torch_jit_policy("model.pt", device)
     return policy
 
 
 if __name__ == "__main__":
-    args = get_args()
     # task, sim_device
-    policy: ActorCriticRMA = load_policy(args, "./checkpoint/model_10000.pt")
+    policy= load_policy("./checkpoint/model_10000.pt")
 
     test_dog = Dog(loaded_policy=policy)
